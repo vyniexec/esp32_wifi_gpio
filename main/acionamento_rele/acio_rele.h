85f8d9c843eb28f72b12d1cb5280ba6795b33a6d
Vinicius Santos <109113022+vyniexec@users.noreply.github.com>
2023-03-14 19:55:31 -0300

    Primeira versão de comunição wifi acionando um relé

--- main/acionamento_rele/acio_rele.h
+++ main/acionamento_rele/acio_rele.h
@@ -2,6 +2,7 @@
 #define __ACIO_RELE__H__
 #include "../wifi/wifi.h"
 #include "../includes.h"
+
 esp_err_t led2on_get_handler(httpd_req_t *req);
 esp_err_t led2off_get_handler(httpd_req_t *req);
 httpd_handle_t start_webserver(void);
